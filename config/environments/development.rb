78ffddac95d251748552aeb1f9fd09316b1b7218
LiSA6900 <amnos.1999.arasick@gmail.com>
2023-03-25 14:50:46 +0000

    [Add]jQueryを使用する設定

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,6 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  
+  config.hosts << "66427f89fd0549c6baf4beba59e779c8.vfs.cloud9.ap-northeast-1.amazonaws.com"
 end
